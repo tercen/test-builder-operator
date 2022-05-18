dca7167c386dd13326d4bd6e9e81ec4205d24b70
Thiago Monteiro <tsmonteiro@gmail.com>
2022-05-18 14:07:45 +0000

    Generated test data

---
+++ test_utils.R
@@ -0,0 +1,34 @@
+#{
+#  "kind": "OperatorUnitTest",
+#  "name": "test1",
+#  "namespace": "test",
+#  "inputDataUri": "crabs-long.csv",
+#  "outputDataUri": ["result.csv"],
+#  "columns": ["sex"],
+#  "rows": ["variable"],
+#  "colors": [],
+#  "labels": [],
+#  "yAxis": "measurement",
+#  "xAxis": ""
+#}
+
+
+
+build_test_data <- function(
+  in_table, out_tables,
+  name="test",
+  namespace="test",
+  in_columns=c(),
+  in_rows=c(),
+  in_colors=c(),
+  in_labels=c(),
+  in_yAxis=c(),
+  in_xAxis=c(),
+  columns=c(),
+  rows=c(),
+  colors=c(),
+  labels=c(),
+  yAxis=c(),
+  xAxis=c() ){
+  
+}
