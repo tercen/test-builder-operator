43495854a90d111ce0367396189ce87aa5eb44c9
Thiago Monteiro <tsmonteiro@gmail.com>
2022-05-20 14:04:06 +0000

    docId testing

--- main.R
+++ main.R
@@ -1,11 +1,56 @@
 library(tercen)
+library(tercenApi)
 library(dplyr, warn.conflicts = FALSE)
+library(tim)
+library(jsonlite)
+
+
+read_doc_id <- function(ctx, documentId){
+  
+  docId <- ctx$cselect("documentId")
+  
+  f.names <- tim::load_data(ctx, docId[[1]] )
+  
+  hosp <- read.csv(f.names[1])
+  
+  hosp <- hosp %>%
+    select('Facility.Name', 'Rating.Safety', 'Procedure.Heart.Attack.Value') %>%
+    mutate(.ci=0)
+  
+  return(hosp)
+  
+  
+}
+
+# http://127.0.0.1:5402/test-team/w/644ee03767c11f751a0614ac820a4da0/ds/414a0287-55df-48b6-ac83-825a6a90a7c6
+# options("tercen.workflowId"="644ee03767c11f751a0614ac820a4da0")
+# options("tercen.stepId"="414a0287-55df-48b6-ac83-825a6a90a7c6")
 
 ctx = tercenCtx()
 
-ctx %>%
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+tbl<- ctx %>%
+  read_doc_id() %>%
   ctx$addNamespace() %>%
   ctx$save()
+
+# http://127.0.0.1:5402/test-team/w/644ee03767c11f751a0614ac820a4da0/ds/414a0287-55df-48b6-ac83-825a6a90a7c6
+# source("/home/rstudio/projects/test_builder_operator/test_utils.R")
+
+#build_test_data( tbl, ctx, "auto_generated_1",
+#                 test_folder = "/home/rstudio/projects/test_builder_operator/tests",
+#                 version = '',
+#                 docIdMapping = c("32d4b2986b98f3ebd5b5baa990000148"="hospitals.csv.zip"))
+
+# -----
+# Compare restuls
+#check_test_local( tbl, "auto_generated_1", 
+#                  test_folder = "/home/rstudio/projects/test_builder_operator/tests", 
+#                              metric="r2",
+#                              absTol=0, relTol=1e-15, r2=1 )
+
+# -----
+
+# Next steps
+# 1 - Run test locally
+# 2 - Run test at the function level
+# 3 - Create a small report to append to the README.md (single line, might make a more extensive TEST.md)
--- test_utils.R
+++ test_utils.R
@@ -1,16 +1,21 @@
+library(stringr)
 
-build_test_data <- function( out_table, ctx, test_name, test_folder = NULL, version = '' ){
+
+build_test_data <- function( out_table, ctx, test_name, 
+                             test_folder = NULL, version = '',
+                             docIdMapping=c()){
   if( is.null(test_folder)){
     test_folder <- paste0( getwd(), '/tests' )
   }
   
   namespace <- ctx$namespace
   
-  
-  save(tbl,file= file.path(test_folder, paste0(test_name, '.Rda')) )
+  testTbl <- tbl
+  save(testTbl,file= file.path(test_folder, paste0(test_name, '.Rda')) )
   proj_names <- ctx$names
   
-  select_names <- c()
+  # Always present even if there is nothing set for it
+  select_names <- c(".y", ".ci",".ri")
   yAxis <- ''
   xAxis <- ''
 
@@ -18,26 +23,30 @@ build_test_data <- function( out_table, ctx, test_name, test_folder = NULL, vers
   has_x <- FALSE  
   
   # Check whether y and x axis are set
-  if( ".y" %in% proj_names){
-    select_names <- append(select_names, ".y")
-    yAxis <- "y_values"
-    has_y <- TRUE
-  }
+  # if( ".y" %in% proj_names){
+  #   select_names <- append(select_names, ".y")
+  #   yAxis <- "y_values"
+  #   has_y <- TRUE
+  # }
   if( ".x" %in% proj_names){
     select_names <- append(select_names, ".x")  
     xAxis <- "x_values"
     has_x <- TRUE
   }
-  if( ".ci" %in% proj_names){select_names <- append(select_names, ".ci")  }
-  if( ".ri" %in% proj_names){select_names <- append(select_names, ".ri")  }
+  # if( ".ci" %in% proj_names){select_names <- append(select_names, ".ci")  }
+  # if( ".ri" %in% proj_names){select_names <- append(select_names, ".ri")  }
   
   # Select, if available, .y, .x, .ci and.ri and corresponding row and column tables
   in_tbl <- ctx$select(select_names) 
   in_rtbl <- ctx$rselect()
   in_ctbl <- ctx$cselect()
+
+  
   
   if(has_y == TRUE){
     in_tbl <- in_tbl%>% rename("y_values"=".y") 
+  }else{
+    in_tbl <- in_tbl%>% select(-".y")
   }
   
   if(has_x == TRUE){
@@ -51,25 +60,41 @@ build_test_data <- function( out_table, ctx, test_name, test_folder = NULL, vers
   # .all -> Empty row or column projection table
   if( names(in_rtbl) != ".all" ){
     in_rtbl <- in_rtbl %>% mutate( .ri=seq(0,nrow(.)-1) )
-    in_tbl <- dplyr::left_join( in_tbl, in_rtbl, by=".ri" ) %>%
+    in_tbl <- dplyr::full_join( in_tbl, in_rtbl, by=".ri" ) %>%
       select(-".ri") 
     has_row_tbl <- TRUE
   }else{
     in_tbl <- select(in_tbl, -".ri")
-    out_table <- out_table %>% select(-".ri")
+    tryCatch({
+      out_table <- out_table %>% select(-".ri")
+    }, error=function(cond){
+      # Ignore, no .ri column in result
+    })
   }
   
   if( names(in_ctbl) != ".all" ){
     in_ctbl <- in_ctbl %>% mutate( .ci=seq(0,nrow(.)-1) )
-    in_tbl <- dplyr::left_join( in_tbl, in_ctbl, by=".ci" ) %>%
+    in_tbl <- dplyr::full_join( in_tbl, in_ctbl, by=".ci" ) %>%
       select(-".ci")
 
     has_col_tbl <- TRUE
   }else{
     in_tbl <- select(in_tbl, -".ci")
-    out_table <- out_table %>% select(-".ci")
+    tryCatch({
+      out_table <- out_table %>% select(-".ci")
+    }, error=function(cond){
+      # Ignore, no .ci column in result
+    })
   }
   
+  if( length(docIdMapping) > 0 ){
+    # Find documentId instances and replace them
+    lapply(docIdMapping, function(x){
+        in_tbl %>%
+        mutate_all( ~str_replace(., unlist(names(docIdMapping)[[1]]),
+                                 docIdMapping)[[1]])
+    })
+  }
   
   # If no version is supplied, try to get the latest version in the repo,
   # and failing that, the latest commit
@@ -134,3 +159,131 @@ build_test_data <- function( out_table, ctx, test_name, test_folder = NULL, vers
   write(json_data, json_file) 
   write.csv(in_tbl, in_tbl_file, row.names = FALSE)
 }
+
+
+check_test_local <- function( out_table, test_name, test_folder = NULL, 
+                              metric="eq",
+                              absTol=0, relTol=0, r2=0 ){
+  if( is.null(test_folder)){
+    test_folder <- paste0( getwd(), '/tests' )
+  }
+  # Loads a variable called testTbl
+  load(file.path(test_folder, paste0(test_name, '.Rda')) )
+  
+  
+  compare_results(out_table, testTbl, metric = metric,
+                  absTol=absTol, relTol=relTol, r2=r2)
+  
+  
+  # If no error occurs ...
+  print( paste0("Test ", test_name, " completed successfuly.\n")  )
+}
+
+# metric: c("eq", "tol", "r2")
+compare_results <- function( newTbl, testTbl, metric="eq",
+                             absTol=0, relTol=0, r2=0){
+  newCols <- names(newTbl)
+  testCols <- names(testTbl)
+  
+  if( length(newCols) != length(testCols) ){
+    err_msg <- paste0("ERROR: Mismatch between number of columns\n\n",
+                     "The new result has ", length(newCols), "columns [",
+                     paste(newCols, collapse = ", "), "]\n\n",
+                     "The saved result has ", length(testCols), "columns [",
+                     paste(testCols, collapse = ", "), "]\n\n")
+    
+    
+    stop(err_msg)
+  }
+  
+  if( nrow(newTbl) != nrow(testTbl) ){
+    err_msg <- paste0("ERROR: Mismatch between number of rows.\n\n",
+                     "The new result has ", nrow(newTbl), "rows.\n\n",
+                     "The saved result has ", nrow(testTbl), "rows.\n\n")
+    stop(err_msg)
+  }
+  
+  if(metric == "eq"){
+    comparetbl_eq_tol(newTbl, testTbl, newCols, relTol=NULL, absTol=0) 
+  }
+  
+  if(metric == "tol"){
+    comparetbl_eq_tol(newTbl, testTbl, newCols, relTol=relTol, absTol=absTol) 
+  }
+  
+  if(metric == "r2"){
+    comparetbl_r2(newTbl, testTbl, newCols, test_r2=r2)
+  }
+}
+
+
+
+comparetbl_r2 <- function(newTbl, testTbl, newCols, test_r2=0){
+  dt <- sapply(newTbl, class)
+  for(i in seq(1, length(newCols))){
+    if( !startsWith(newCols[i], ".") && (
+      dt == "integer" || dt == "numeric")){
+      
+      r2 <- cor( newTbl[,i], testTbl[,i]  )[[1]] ** 2
+      
+      if( all(newTbl[,i]==0) && all(testTbl[,i]==0) ){
+        # If both contain only 0's, cor will return NA
+        # For the purposes of this test, this will be considered 1
+        r2 <- 1
+      }
+      
+      if( r2 <  test_r2 ){
+        err_msg <- paste0('Correlation test failed for column ',
+                          newCols[i], " --- ",
+                          "R2 = ", r2)
+        
+        stop(err_msg)
+      }
+      
+    }
+  }
+}
+
+
+comparetbl_eq_tol <- function(newTbl, testTbl, newCols, relTol=NULL, absTol=0){
+  # For the sake of clarity, run the test row-wise
+  # Might update this if performance becomes a problem in large tables
+  
+  dt <- sapply(newTbl, class)
+  
+  for(i in seq(1, nrow(newTbl))){
+    newRow <- newTbl[i,]
+    testRow <- testTbl[i,]
+    
+    for( j in seq(1, length(newCols)) ){
+      dtCol <- dt[j]
+      if( dtCol == "integer" || dtCol == "numeric" ){
+          deltaAbs <- abs(newRow[j] - testRow[j]  )[[1]]
+          deltaRel <- abs(1- (newRow[j][[1]]/testRow[j][[1]]  ))
+          
+          deltaRelInTol <- is.null(relTol) || (
+            (newRow[j] == 0 && testRow[j] == 0) ||
+              (deltaRel <= relTol)
+          )
+          
+          deltaAbsInTol <- deltaAbs <= absTol
+          
+          isEq <- deltaAbsInTol && deltaRelInTol
+      }else{
+        isEq <- newRow[j] == testRow[j]
+      }
+
+      if( !isEq ){
+        err_msg <- paste0('Comparison test failed at row ',
+                         i, " column ", newCols[j], "\n\n.",
+                         "Values: ", newRow[j], "  --  ",
+                         testRow[j])
+        
+        stop(err_msg)
+      }
+    }
+  }
+}
+
+
+
